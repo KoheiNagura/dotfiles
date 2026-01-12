7f8303747fb60ca14efccda6fef092e71b6b2bd0
KoheiNagura <k.nagura.p@gmail.com>
2026-01-12 12:50:49 +0900

    add: lualine 表示設定

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,4 +2,28 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = "VeryLazy",
+  opts = {
+    options = {
+      theme = 'nord',
+      globalstatus = true,
+      component_separators = '',
+      section_separators = '',
+    },
+    sections = {
+      lualine_a = {
+        {
+          function()
+            return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+          end,
+          icon = '󰉋',
+          color = { bg = '#5E81AC', fg = '#ECEFF4' },
+        },
+      },
+      lualine_b = {},
+      lualine_c = { { 'branch', icon = '' } },
+      lualine_x = { { 'diagnostics', sources = { 'nvim_lsp' } } },
+      lualine_y = { 'filetype' },
+      lualine_z = {},
+    },
+  },
 }
