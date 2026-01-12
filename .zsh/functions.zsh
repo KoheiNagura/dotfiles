cca29e052c98a9d2fb1d601758727d5803980ce3
KoheiNagura <k.nagura.p@gmail.com>
2026-01-12 09:47:37 +0900

    add: yazi終了後に最後のディレクトリへcd

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -57,3 +57,11 @@ wd() {
     [ -n "$selected" ] && cd "{$selected/#~/$HOME}"
 }
 
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
