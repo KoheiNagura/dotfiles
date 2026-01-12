0f8599273cc7afce7aa43d32b263b0fce83c4a25
KoheiNagura <k.nagura.p@gmail.com>
2026-01-12 12:50:56 +0900

    add: scrollbar設定拡張（gitsigns/hlslens統合）

---
+++ .config/nvim/lua/plugins/nvim-hlslens.lua
@@ -0,0 +1,8 @@
+return {
+  "kevinhwang91/nvim-hlslens",
+  event = "VeryLazy",
+  opts = {
+    calm_down = true,
+    nearest_only = true
+  }
+}
--- .config/nvim/lua/plugins/nvim-scrollbar.lua
+++ .config/nvim/lua/plugins/nvim-scrollbar.lua
@@ -1,4 +1,31 @@
 return {
   "petertriho/nvim-scrollbar",
   event = "VeryLazy",
+  dependencies = {
+    "lewis6991/gitsigns.nvim",
+    "kevinhwang91/nvim-hlslens",
+  },
+  config = function()
+    require("scrollbar").setup({
+      show = true,
+      show_in_active_only = false,
+      handle = {
+        blend = 30,
+        hide_if_all_visible = false,
+      },
+      excluded_buftypes = {
+        "terminal",
+      },
+      handlers = {
+        cursor = true,
+        diagnostic = true,
+        gitsigns = true,
+        search = true,
+        handle = true,
+      },
+    })
+
+    require("scrollbar.handlers.gitsigns").setup()
+    require("scrollbar.handlers.search").setup()
+  end,
 }
