9c526be0607af7874ff92772b1e96b8668399de7
KoheiNagura <k.nagura.p@gmail.com>
2026-01-12 12:51:04 +0900

    add: blamer.nvim（インラインでblame表示）

---
+++ .config/nvim/lua/plugins/blamer.lua
@@ -0,0 +1,13 @@
+return {
+  "APZelos/blamer.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  keys = {
+    { "<leader>gb", "<cmd>BlamerToggle<CR>", desc = "Toggle Git Blame" },
+  },
+  init = function()
+    vim.g.blamer_enabled = 1
+    vim.g.blamer_delay = 500
+    vim.g.blamer_relative_time = 1
+    vim.g.blamer_show_in_insert_modes = 0
+  end,
+}
